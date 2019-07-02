5dc3738d675f8379d53a74ca3d62793c43243abf
tra77 user <a08tra77@node166.galileo.cineca.it>
2019-07-02 16:42:25 +0200

    Add first MPI exercises

---
+++ Exercises/MPI/Example/job.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#-------------------- hw section -------------------------------
+
+#### Ask for a number of nodes each with cpus "ntasks-per-node" 
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=16
+##SBATCH --mem=10GB
+ 
+#### Maximum length of the job (hh:mm:ss)
+#SBATCH --time=3:00
+
+#-------------------- partition system -------------------------------
+
+#### Partition of submission (reservation for the school, comment "qos" and "reservation" lines to submit as a regular user)
+#SBATCH --partition=gll_usr_prod
+##SBATCH --qos=
+##SBATCH --reservation= 
+
+# --------------- accounting/budget  -----------------------------
+
+#### account number (type saldo -b)
+#SBATCH --account=train_scR2019 
+
+# ---------------- other info ------------------------------------
+
+#### File for standard output and error
+#SBATCH --output=hello.out
+#SBATCH --error=hello.err
+
+#### Job name
+##SBATCH --job-name=hello_my_friend
+
+#### send email to the following address
+##SBATCH --mail-user= 
+
+#### send email after abort or end
+##SBATCH --mail-type= 
+
+# -----------end of SLURM keywords section -------------------------
+
+module load autoload intelmpi
+
+srun --mpi=pmi2 ./mpi-example.x
+
+
+
+
+
--- Exercises/MPI/Exercise 2/main.c
+++ Exercises/MPI/Exercise 2/main.c
@@ -1,6 +1,6 @@
 #include <assert.h>
 #include <stdio.h>
-#include <stddef.h>
+#include <stdlib.h>
 #include <mpi.h>
 
 /************ Macros and Typedefs **/
@@ -12,12 +12,18 @@
 #define __error(_Fmt, ...) \
     fprintf(stderr, "[!] " _Fmt "\n", __VA_ARGS__)
 
+#define PAYLOAD_SIZE 64
+
 /************ Functions           **/
 mpi_error_status_t main(int argc, char** argv)
 {
+    MPI_Status recv_status;
     mpi_error_status_t status;
     int comm_proc_rank, comm_group_size;
-    int data;
+    float* payload;
+    float* recv_buffer;
+    float payload_reductio;
+    int i;
 
     /** @region Initialization */
     status = MPI_Init(&argc, &argv);
@@ -33,13 +39,26 @@ mpi_error_status_t main(int argc, char** argv)
     switch (comm_proc_rank)
     {
         case 0:
-            data = 0;
-            MPI_Send(&data, 1, MPI_INT, 1, 1337, MPI_COMM_WORLD);
+            payload = malloc(PAYLOAD_SIZE * sizeof(float));
+
+            for (i = 1; i <= PAYLOAD_SIZE; i++)
+                payload[i - 1] = i * i;
+
+            MPI_Send(payload, PAYLOAD_SIZE, MPI_FLOAT, 1, 1337, MPI_COMM_WORLD);
+
             break;
         case 1:
-            data = 1;
-            MPI_Send(&data, 1, MPI_INT, 0, 1337, MPI_COMM_WORLD);
-            __info("Process %d receives %d from process 0", comm_proc_rank, data);
+            recv_buffer = malloc(PAYLOAD_SIZE * sizeof(float));
+
+            MPI_Recv(recv_buffer, PAYLOAD_SIZE, MPI_FLOAT, 0, 1337, MPI_COMM_WORLD, &recv_status);
+	    
+            payload_reductio = 0;
+                        
+            for (i = 0; i < PAYLOAD_SIZE; i++)
+                payload_reductio += recv_buffer[i];
+
+            __info("Process %d receives payload with reduction = %f", comm_proc_rank, payload_reductio); 
+
             break;
         default:
             __error("Invalid rank.", NULL);
@@ -52,6 +71,9 @@ mpi_error_status_t main(int argc, char** argv)
     status = MPI_Finalize();
 
     assert(status == MPI_SUCCESS);
+    
+    free(payload);
+    free(recv_buffer);
 
     return status;
 }
